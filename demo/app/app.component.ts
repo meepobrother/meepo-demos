9334a20243077aecaf3b462d0345af24666665b3
15138730370 <1037483576@qq.com>
2018-01-04 12:49:13 +0800

    back

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { EventService } from 'meepo-event';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() { }
-
-  title = 'app';
+  constructor(
+    public event: EventService
+  ) {
+    const now = new Date();
+    const version = new Date(now.getFullYear(), now.getMonth() + 1, now.getDate()).getTime();
+    this.event.checkVersion(version);
+  }
 }
